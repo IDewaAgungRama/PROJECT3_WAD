09c638a630ac2aff81f8cbeb56d4f16596d497e8
IDewaAgungRama <116362990+IDewaAgungRama@users.noreply.github.com>
2023-11-02 19:19:12 +0700

    Add files via upload

---
+++ Latihanindex.php
@@ -0,0 +1,35 @@
+<?php
+$conn = mysqli_connect("localhost", "root","","wad_project3" );
+$result = mysqli_query($conn,"SELECT*FROM students");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <table>
+        <tr>
+          <th>ID</th>
+          <th>NAME</th>
+          <th>NIM</th>
+          <th>ADDRESS</th>
+        </tr>
+<?php while ($data=mysqli_fetch_object($result)){
+  ?>
+        <tr>
+          <td><?php echo $data->id?></td>
+          <td><?php echo $data->name?></td>
+          <td><?php echo $data->nim?></td>
+          <td><?php echo $data->address?></td>
+        </tr>
+<?php
+}
+?>
+</table>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,4 @@
+<?php
+$conn = mysqli_connect("localhost", "root","","wad_project3" );
+$result = mysqli_query($conn,"SELECT*FROM students");
+?>
